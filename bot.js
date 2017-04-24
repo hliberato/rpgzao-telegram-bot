a1ef77f930ca7ecadd1f4cdacbb2cb4692bca5fb
Henrique Liberato <henriquebmx@gmail.com>
2017-04-24 17:49:34 -0300

    migrando de rails para node

---
+++ bot.js
@@ -0,0 +1,22 @@
+var token = process.env.TOKEN;
+
+var Bot = require('node-telegram-bot-api');
+var bot;
+
+if(process.env.NODE_ENV === 'production') {
+  bot = new Bot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+}
+else {
+  bot = new Bot(token, { polling: true });
+}
+
+console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
+
+bot.onText(/^/, function (msg) {
+  var name = msg.from.first_name;
+  bot.sendMessage(msg.chat.id, 'Bem vindo ao Bot do grupo RPGzão, ' + name + '!').then(function () {
+  });
+});
+
+module.exports = bot;
